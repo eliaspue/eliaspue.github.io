899b7ec9283b92a3d3de98ce5435818cfe68e5a6
Esteban ELIAS PUEYO <esteban.Elias-Pueyo@etu.univ-grenoble-alpes.fr>
2024-06-29 01:00:07 +0200

    Modifying + add little text

--- scripts.js
+++ scripts.js
@@ -13,4 +13,12 @@ document.addEventListener('DOMContentLoaded', function() {
             card.classList.toggle('flipped');
         });
     });
+
+    // Initialiser les barres de compétences
+    const skillLevels = document.querySelectorAll('.skill-level');
+    skillLevels.forEach(skill => {
+        const width = skill.style.width;
+        skill.style.setProperty('--skill-width', width);
+        skill.style.width = '0';
+    });
 });
