be68a2248050eb7d494f81407c633b119e8e3ad8
Esteban ELIAS PUEYO <esteban.Elias-Pueyo@etu.univ-grenoble-alpes.fr>
2024-06-30 18:12:35 +0200

    ajouts CV

--- scripts.js
+++ scripts.js
@@ -1,11 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const contactForm = document.getElementById('contact-form');
-    contactForm.addEventListener('submit', function(e) {
-        e.preventDefault();
-        alert('Merci pour votre message !');
-        // Vous pouvez ajouter ici du code pour envoyer les données du formulaire au serveur
-        contactForm.reset();
-    });
 
     const cards = document.querySelectorAll('.card-inner');
     cards.forEach(card => {
